0ae027301c97ffe3b0de8998d0f922d98d9e10d3
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-03-19 20:42:40 -0400

    Corrected the bug with GPU transfer

--- ConvStackClassifier.py
+++ ConvStackClassifier.py
@@ -12,14 +12,14 @@ class NeuralNet(nn.Module):
         if len(numberOfConvolutionKernelsList) != len(maxPoolingKernelList):
             raise RuntimeError("ConvStackClassifier.NeuralNet.__init__(): len(numberOfConvolutionKernelsList) ({}) != len(maxPoolingKernelList) ({})".format(len(numberOfConvolutionKernelsList), len(maxPoolingKernelList)) )
 
-        self.convLayers = []
+        #self.convLayers = []
         poolingReductionFactor = maxPoolingKernelList[0]
-        self.convLayers.append(nn.Sequential(
+        self.add_module('conv0', nn.Sequential(
             nn.Conv2d(3, numberOfConvolutionKernelsList[0], kernel_size=7, padding=3),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(maxPoolingKernelList[0])))
         for layerNdx in range(1, len(numberOfConvolutionKernelsList)):
-            self.convLayers.append(nn.Sequential(
+            self.add_module('conv{}'.format(layerNdx), nn.Sequential(
                 nn.Conv2d(numberOfConvolutionKernelsList[layerNdx - 1], numberOfConvolutionKernelsList[layerNdx], kernel_size=7, padding=3),
                 nn.ReLU(inplace=True),
                 nn.MaxPool2d(maxPoolingKernelList[layerNdx])))
@@ -29,14 +29,20 @@ class NeuralNet(nn.Module):
         self.linear1 = nn.Linear(self.lastLayerImageSize * self.lastLayerImageSize *
                                  self.lastLayerNumberOfChannels, classesNbr)
         self.dropout = nn.Dropout2d(p=dropoutRatio)
-
+        self.numberOfConvolutionLayers = len(numberOfConvolutionKernelsList)
 
     def forward(self, inputs):
-        activation = self.convLayers[0](inputs)
-        for layerNdx in range(1, len(self.convLayers)):
-            activation = self.convLayers[layerNdx](activation)
+        #moduleNames, modules = self.named_childer()
+        activation = self.conv0(inputs)
+        for layerNdx in range(1, self.numberOfConvolutionLayers):
+            layerName = 'conv{}'.format(layerNdx)
+            # Find the module by its name
+            for name, module in self.named_children():
+                if name == layerName:
+                    activation = module(activation)
+            #activation = self.convLayers[layerNdx](activation)
 
         vector = activation.view(-1, self.lastLayerImageSize * self.lastLayerImageSize * self.lastLayerNumberOfChannels)
         drop = self.dropout(vector)
         outputLin = self.linear1(drop)
-        return F.log_softmax(outputLin)
+        return F.log_softmax(outputLin, dim=0)
