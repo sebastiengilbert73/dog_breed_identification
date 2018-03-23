df7e3f3e72d2bb7d0c53c1f8868e768fff5758b3
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-03-23 18:26:12 -0400

    ConvStackClassifier.py: Use an OrderedDict, then Sequential(orderedDic)

--- ConvStackClassifier.py
+++ ConvStackClassifier.py
@@ -2,30 +2,38 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+from collections import OrderedDict
 
 class NeuralNet(nn.Module):
-    def __init__(self, numberOfConvolutionKernelsList, maxPoolingKernelList, classesNbr,
-                 imageSize, dropoutRatio=0.5):
+    #def __init__(self, numberOfConvolutionKernelsList, maxPoolingKernelList, numberOfInputChannels,
+    def __init__(self, numberOfConvolutions_KernelSize_Pooling_List, numberOfInputChannels,
+                 classesNbr, imageSize, dropoutRatio=0.5):
         super(NeuralNet, self).__init__()
-        if len(numberOfConvolutionKernelsList) < 1:
-            raise RuntimeError("ConvStackClassifier.NeuralNet.__init__(): numberOfConvolutionKernelsList is empty")
-        if len(numberOfConvolutionKernelsList) != len(maxPoolingKernelList):
-            raise RuntimeError("ConvStackClassifier.NeuralNet.__init__(): len(numberOfConvolutionKernelsList) ({}) != len(maxPoolingKernelList) ({})".format(len(numberOfConvolutionKernelsList), len(maxPoolingKernelList)) )
 
-        self.convLayers = []
-        poolingReductionFactor = maxPoolingKernelList[0]
-        self.convLayers.append(nn.Sequential(
-            nn.Conv2d(3, numberOfConvolutionKernelsList[0], kernel_size=7, padding=3),
+        if len(numberOfConvolutions_KernelSize_Pooling_List) < 1:
+            raise RuntimeError("ConvStackClassifier.NeuralNet.__init__(): The number of convolution layers is 0")
+        self.lastLayerImageSize = imageSize
+        layersDict = OrderedDict()
+
+        layersDict['conv0'] = nn.Sequential(
+            nn.Conv2d(numberOfInputChannels, numberOfConvolutions_KernelSize_Pooling_List[0][0],
+                      numberOfConvolutions_KernelSize_Pooling_List[0][1],
+                      padding=(int) (numberOfConvolutions_KernelSize_Pooling_List[0][1]/2) ),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(maxPoolingKernelList[0])))
-        for layerNdx in range(1, len(numberOfConvolutionKernelsList)):
-            self.convLayers.append(nn.Sequential(
-                nn.Conv2d(numberOfConvolutionKernelsList[layerNdx - 1], numberOfConvolutionKernelsList[layerNdx], kernel_size=7, padding=3),
+            nn.MaxPool2d(numberOfConvolutions_KernelSize_Pooling_List[0][2]))
+        self.lastLayerImageSize = (int) (self.lastLayerImageSize/ numberOfConvolutions_KernelSize_Pooling_List[0][2])
+        for layerNdx in range(1, len(numberOfConvolutions_KernelSize_Pooling_List)):
+            layersDict['conv{}'.format(layerNdx)] = nn.Sequential(
+                nn.Conv2d(numberOfConvolutions_KernelSize_Pooling_List[layerNdx - 1][0], numberOfConvolutions_KernelSize_Pooling_List[layerNdx][0],
+                          numberOfConvolutions_KernelSize_Pooling_List[layerNdx][1],
+                          padding=(int)(numberOfConvolutions_KernelSize_Pooling_List[layerNdx][1] / 2)),
                 nn.ReLU(inplace=True),
-                nn.MaxPool2d(maxPoolingKernelList[layerNdx])))
-            poolingReductionFactor *= maxPoolingKernelList[layerNdx]
-        self.lastLayerImageSize = int(imageSize / poolingReductionFactor)
-        self.lastLayerNumberOfChannels = numberOfConvolutionKernelsList[-1]
+                nn.MaxPool2d(numberOfConvolutions_KernelSize_Pooling_List[layerNdx][2]))
+            self.lastLayerImageSize = (int)(self.lastLayerImageSize / numberOfConvolutions_KernelSize_Pooling_List[layerNdx][2])
+
+        self.convLayers = nn.Sequential(layersDict)
+        self.lastLayerNumberOfChannels = numberOfConvolutions_KernelSize_Pooling_List[-1][0]
+        self.numberOfConvolutionLayers = len(layersDict)
         self.linear1 = nn.Linear(self.lastLayerImageSize * self.lastLayerImageSize *
                                  self.lastLayerNumberOfChannels, classesNbr)
         self.dropout = nn.Dropout2d(p=dropoutRatio)
@@ -33,7 +41,7 @@ class NeuralNet(nn.Module):
 
     def forward(self, inputs):
         activation = self.convLayers[0](inputs)
-        for layerNdx in range(1, len(self.convLayers)):
+        for layerNdx in range(1, self.numberOfConvolutionLayers):
             activation = self.convLayers[layerNdx](activation)
 
         vector = activation.view(-1, self.lastLayerImageSize * self.lastLayerImageSize * self.lastLayerNumberOfChannels)
